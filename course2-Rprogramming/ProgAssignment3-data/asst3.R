a2df30e1ed8dd687ecfa83a635b22369f73e2b06
Irena Papst <ip98@cornell.edu>
2018-06-22 12:39:10 -0400

    made unit testing easier in asst3.R

--- course2-Rprogramming/ProgAssignment3-data/asst3.R
+++ course2-Rprogramming/ProgAssignment3-data/asst3.R
@@ -3,6 +3,11 @@
 ##
 ## Script to test functions written for asst 3
 
+## Set number of test
+## 1: best()
+## 2: rankhospital()
+test <- 2
+
 ############
 ## PART 0 ##
 ############
@@ -22,37 +27,45 @@
 ############
 
 ## Testing best()
-# source("best.R")
-# state <- "TX"
-# outcome <- "heart attack"
-# print(best(state, outcome))
-# state <- "TX"
-# outcome <- "heart failure"
-# print(best(state, outcome))
-# state <- "MD"
-# outcome <- "heart attack"
-# print(best(state, outcome))
-# state <- "MD"
-# outcome <- "pneumonia"
-# print(best(state, outcome))
-# state <- "BB"
-# outcome <- "heart attack"
-# try(print(best(state, outcome)))
-# state <- "NY"
-# outcome <- "hert attack"
-# try(print(best(state, outcome)))
+if (test==1){
+  source("best.R")
+  state <- "TX"
+  outcome <- "heart attack"
+  print(best(state, outcome))
+  state <- "TX"
+  outcome <- "heart failure"
+  print(best(state, outcome))
+  state <- "MD"
+  outcome <- "heart attack"
+  print(best(state, outcome))
+  state <- "MD"
+  outcome <- "pneumonia"
+  print(best(state, outcome))
+  state <- "BB"
+  outcome <- "heart attack"
+  try(print(best(state, outcome)))
+  state <- "NY"
+  outcome <- "hert attack"
+  try(print(best(state, outcome)))
+}
+
+############
+## PART 2 ##
+############
 
 ## Testing rankhospital.R
-# source("rankhospital.R")
-state <- "TX"
-outcome <- "heart failure"
-rank <- 4
-print(rankhospital(state, outcome, rank))
-state <- "MD"
-outcome <- "heart attack"
-rank <- "worst"
-print(rankhospital(state, outcome, rank))
-state <- "MN"
-outcome <- "heart attack"
-rank <- 5000
-print(rankhospital(state, outcome, rank))
+if (test==2){
+  source("rankhospital.R")
+  state <- "TX"
+  outcome <- "heart failure"
+  rank <- 4
+  print(rankhospital(state, outcome, rank))
+  state <- "MD"
+  outcome <- "heart attack"
+  rank <- "worst"
+  print(rankhospital(state, outcome, rank))
+  state <- "MN"
+  outcome <- "heart attack"
+  rank <- 5000
+  print(rankhospital(state, outcome, rank))
+}
